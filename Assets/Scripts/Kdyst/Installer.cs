4a46932878903c683fd303363d0fe8c3545c9624
kdyst <k1671k@naver.com>
2022-10-17 00:47:55 +0900

    [Kim] 미리 입력받은 값을 바탕으로 타일 자동 생성

--- Assets/Scripts/Kdyst/Installer.cs
+++ Assets/Scripts/Kdyst/Installer.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Installer : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] tileSet;
+
+    public SerializableMatrix<int> tileMap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < tileMap.rows.Count; x++)
+            for(int z = 0; z < tileMap.rows[0].values.Count; z++)
+            {
+                GameObject target = Instantiate(tileSet[tileMap[x, z]], gameObject.transform);
+                target.transform.Translate(Vector3.right * x + Vector3.forward * z);
+                target.SetActive(true);
+            }
     }
 
     // Update is called once per frame

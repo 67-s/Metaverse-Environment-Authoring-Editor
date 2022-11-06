24aa5887ed4d736dfa144d6c0d348dfc23a91e7e
seungchul <secul5972@naver.com>
2022-11-06 23:42:31 +0900

    [Oh] 방 참가자의 방 목록

---
+++ Assets/Scripts/Seung/RoomListScrollView.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RoomListScrollView : MonoBehaviour
+{
+	public GameObject roomContent;
+	static int roomCount = 0;
+
+	public void WhenRoomCreated()
+	{
+		GameObject newRoomContent = Instantiate(roomContent);
+		Button check = newRoomContent.GetComponentInChildren<Button>();
+
+		newRoomContent.transform.SetParent(transform);
+		check.onClick.AddListener(OnChkClick);
+
+		newRoomContent.SetActive(true);
+	}
+
+	public void OnChkClick()
+	{
+		var currObject = EventSystem.current.currentSelectedGameObject;
+		TMP_Text btnText = currObject.GetComponentInChildren<TMP_Text>();
+		if (btnText.text.Equals("O"))
+		{
+			roomCount--;
+			btnText.text = "";
+		}
+		else if (roomCount == 0)
+		{
+			btnText.text = "O";
+			roomCount++;
+		}
+	}
+}

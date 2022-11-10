edd578f68d2e697ea57c0079e8bd58b226a491cc
yhchon11 <sgu1638@naver.com>
2022-11-11 02:00:13 +0900

    [yhchon11]character_animation_jump

--- Assets/Scripts/Sol/PersonBehaviour.cs
+++ Assets/Scripts/Sol/PersonBehaviour.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Bolt;
@@ -7,7 +7,13 @@ public class PersonBehaviour : EntityEventListener<IPersonState>
 {
     Rigidbody rigid;
     Animator animator;
-
+    //yhchon11
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yForce = 10f;
+    [SerializeField] float zVelocity = 2f;
+    [SerializeField] float overAir = 1.7f;
+    private bool isJump = false;
+    //
     public override void Attached()
     {
         state.SetTransforms(state.PersonTransform, transform);
@@ -19,7 +25,12 @@ public class PersonBehaviour : EntityEventListener<IPersonState>
     {
         var speed = 4f;
         var movement = Vector3.zero;
-
+        //yhchon11
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * xVelocity;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * zVelocity;
+        float zump = Input.GetAxis("Jump") * Time.deltaTime * yForce;
+        transform.Translate(new Vector3(horizontal, 0, vertical));
+        //
         if (Input.GetKey(KeyCode.W)) { movement.z += 1; }
         if (Input.GetKey(KeyCode.S)) { movement.z -= 1; }
         if (Input.GetKey(KeyCode.A)) { movement.x -= 1; }
@@ -44,6 +55,23 @@ public class PersonBehaviour : EntityEventListener<IPersonState>
             animator.SetBool("isRun", false);
         }
 
+        //yhchon11
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isJump)
+            {
+                StartCoroutine(Jump());
+            }
+        }
+
+        IEnumerator Jump()
+        {
+            isJump = true;
+            rigid.AddForce(Vector3.up * yForce, ForceMode.Impulse);
+            yield return new WaitForSeconds(overAir);//받는 파트
+            isJump = false;
+        }//coroutine: n초 동안 cpu에게 권한을 넘긴다, 그후 받는다
+        //
     }
 
 }

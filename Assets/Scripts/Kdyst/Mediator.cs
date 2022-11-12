86884875c5137c913d939d3eecac4eef77ce68c7
kdyst <k1671k@naver.com>
2022-11-12 17:02:06 +0900

    [Kim] 맵 생성기를 Scene1에 추가 & 맵 저작 UI와 연결

---
+++ Assets/Scripts/Kdyst/Mediator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mediator : MonoBehaviour
+{
+    [SerializeField] private GameObject _element;
+    public GameObject element { get { return _element; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

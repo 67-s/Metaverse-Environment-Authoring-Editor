f6467719d0fa283bd9bdba784111803adeac7e2f
yhchon11 <sgu1638@naver.com>
2022-11-21 12:52:02 +0900

    [yhchon11] cam controller

--- Assets/Scripts/yhchon11/CamController.cs
+++ Assets/Scripts/yhchon11/CamController.cs
@@ -6,16 +6,33 @@ public class CamController : MonoBehaviour
 {
     public float cameraSpeed = 5.0f;
 
-    public GameObject player;
+    //public GameObject player;
+
+    //yhchon11
+    [SerializeField] Transform player;
+    [SerializeField] float smoothing = 0.2f;
+    [SerializeField] Vector2 minCameraBoundary;
+    [SerializeField] Vector2 maxCameraBoundary;
+    //
 
     void start()
     {
 
     }
-    void LateUpdate()
+    /*void LateUpdate()
     {
         Vector3 dir = player.transform.position - this.transform.position;
         Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, dir.z * cameraSpeed * Time.deltaTime);
         this.transform.Translate(moveVector);
+    }*/
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+
+        targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
+        //targetPos.z = Mathf.Clamp(targetPos.z, minCameraBoundary.y, maxCameraBoundary.y);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
     }
 }

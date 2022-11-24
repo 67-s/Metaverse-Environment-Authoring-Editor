44031bb424ca35d5d2311be9e3882a84f24bc410
yhchon11 <sgu1638@naver.com>
2022-11-25 01:03:42 +0900

    [yhchon11]lagging_fail

--- Assets/Scripts/Sol/PersonBehaviour.cs
+++ Assets/Scripts/Sol/PersonBehaviour.cs
@@ -72,5 +72,13 @@ public class PersonBehaviour : EntityEventListener<IPersonState>
         */
 
     }
+    //yhchon11
+    private GameObject Target;
+    public float CameraZ = -10;
+    private void FixedUpdate()
+    {
+        Vector3 TargetPos = new Vector3(Target.transform.position.x, Target.transform.position.y, CameraZ);
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 2f);
+    }
 
 }

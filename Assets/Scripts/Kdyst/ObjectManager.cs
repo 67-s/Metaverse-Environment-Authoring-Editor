684b7ea78d41326e8c6f9ea219952ce520bceee2
kdyst <k1671k@naver.com>
2022-11-29 20:16:44 +0900

    [Kim] ObjectManager 제작

---
+++ Assets/Scripts/Kdyst/ObjectManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+struct KeyValue
+{
+    public int key;
+    public GameObject gameObject;
+}
+
+public class ObjectManager : MonoBehaviour
+{
+    [SerializeField] private List<KeyValue> prefabs = new();
+    private readonly Dictionary<int, GameObject> mapper = new();
+
+    public GameObject Find(int key)
+    {
+        return mapper[key];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var kv in prefabs)
+        {
+            try
+            {
+                mapper.Add(kv.key, kv.gameObject);
+            }
+            catch(ArgumentException)
+            {
+                Debug.LogError("kdyst/ObjectManager.cs: The key " + kv.key + " alreay exists.");
+            }
+        }
+    }
+
+    public bool actionSpawn = false;
+    public int actionSpawnKey;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(actionSpawn)
+        {
+            actionSpawn = false;
+            Instantiate(this.Find(actionSpawnKey), gameObject.transform);
+        }
+    }
+}
